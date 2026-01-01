801e8824eafa26491473cdeb8edf41e2adb08c1f
Yusuf-Y <23soft1025@isik.edu.tr>
2026-01-01 19:07:56 +0300

    satisfying requirement one

---
+++ hmk1/RangePrinter.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class RangePrinter {
+
+    public static void printRange(int a, int b) {
+        for (int i = a + 1; i < b; i++) {
+            System.out.println(i);
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        printRange(a, b);
+    }
+}
+
